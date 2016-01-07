a2b156b013de96017d0a003c22ad08e267b731ab
Philipp Kraus <philipp.kraus@flashpixx.de>
2016-01-07 12:38:18 +0100

    fix docu

--- src/main/java/lightjason/language/execution/IExecution.java
+++ src/main/java/lightjason/language/execution/IExecution.java
@@ -68,7 +68,6 @@ public interface IExecution
      *
      * @warning must create an individual / local set, because
      * variables will be instantiate locally, so variables must be cloned
-     * @bug add calls overwrite variables within the set, change it to if a variable exists, it will not be replaced
      */
     Set<IVariable<?>> getVariables();
 }
