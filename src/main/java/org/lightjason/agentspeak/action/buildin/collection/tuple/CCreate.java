53c0e395f8a915999758d5d2cc2bae8511e4fc22
Philipp Kraus <philipp.kraus@flashpixx.de>
2016-08-23 08:21:33 +0200

    add tuple action

---
+++ src/main/java/org/lightjason/agentspeak/action/buildin/collection/tuple/CCreate.java
@@ -0,0 +1,71 @@
+/*
+ * @cond LICENSE
+ * ######################################################################################
+ * # LGPL License                                                                       #
+ * #                                                                                    #
+ * # This file is part of the LightJason AgentSpeak(L)                                  #
+ * # Copyright (c) 2015-16, LightJason (info@lightjason.org)                            #
+ * # This program is free software: you can redistribute it and/or modify               #
+ * # it under the terms of the GNU Lesser General Public License as                     #
+ * # published by the Free Software Foundation, either version 3 of the                 #
+ * # License, or (at your option) any later version.                                    #
+ * #                                                                                    #
+ * # This program is distributed in the hope that it will be useful,                    #
+ * # but WITHOUT ANY WARRANTY; without even the implied warranty of                     #
+ * # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                      #
+ * # GNU Lesser General Public License for more details.                                #
+ * #                                                                                    #
+ * # You should have received a copy of the GNU Lesser General Public License           #
+ * # along with this program. If not, see http://www.gnu.org/licenses/                  #
+ * ######################################################################################
+ * @endcond
+ */
+
+package org.lightjason.agentspeak.action.buildin.collection.tuple;
+
+import org.lightjason.agentspeak.action.buildin.IBuildinAction;
+import org.lightjason.agentspeak.language.CCommon;
+import org.lightjason.agentspeak.language.CRawTerm;
+import org.lightjason.agentspeak.language.ITerm;
+import org.lightjason.agentspeak.language.execution.IContext;
+import org.lightjason.agentspeak.language.execution.fuzzy.CFuzzyValue;
+import org.lightjason.agentspeak.language.execution.fuzzy.IFuzzyValue;
+
+import java.util.*;
+
+
+/**
+ * creates a tuple of two elements
+ */
+public final class CCreate extends IBuildinAction
+{
+    /**
+     * ctor
+     */
+    public CCreate()
+    {
+        super( 3 );
+    }
+
+    @Override
+    public final int minimalArgumentNumber()
+    {
+        return 2;
+    }
+
+    @Override
+    public final IFuzzyValue<Boolean> execute( final IContext p_context, final boolean p_parallel, final List<ITerm> p_argument, final List<ITerm> p_return,
+                                               final List<ITerm> p_annotation
+    )
+    {
+        p_return.add( CRawTerm.from(
+                new AbstractMap.SimpleImmutableEntry<>(
+                        CCommon.raw( p_argument.get( 0 ) ),
+                        CCommon.raw( p_argument.get( 1 ) )
+                )
+        ) );
+
+        return CFuzzyValue.from( true );
+    }
+
+}
