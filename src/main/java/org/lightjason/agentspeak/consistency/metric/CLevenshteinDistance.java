3294d45e2ecda4f5031324660e71acf4d99d5765
Philipp Kraus <philipp.kraus@flashpixx.de>
2016-09-27 20:57:28 +0200

    fix distance

--- src/main/java/org/lightjason/agentspeak/consistency/metric/CLevenshteinDistance.java
+++ src/main/java/org/lightjason/agentspeak/consistency/metric/CLevenshteinDistance.java
@@ -38,11 +38,46 @@ import java.util.stream.Stream;
  */
 public final class CLevenshteinDistance implements IMetric
 {
-    @Override
-    public final double calculate( final Stream<? extends ITerm> p_first, final Stream<? extends ITerm> p_second
-    )
+    /**
+     * cost / weight of insert operation
+     */
+    private final double m_insertweight;
+    /**
+     * cost / weight of replace operation
+     */
+    private final double m_replaceweight;
+    /**
+     * cost / weight of delete operation
+     */
+    private final double m_deleteweight;
+
+    /**
+     * ctor
+     */
+    public CLevenshteinDistance()
     {
-        return CLevenshteinDistance.levenshtein(
+        this( 1, 1, 1 );
+    }
+
+    /**
+     * ctor
+     *
+     * @param p_insertweight weight / cost of insert character
+     * @param p_replaceweight weight / cost of replace character
+     * @param p_deleteweight weight / cost of delete character
+     */
+    public CLevenshteinDistance( final double p_insertweight, final double p_replaceweight, final double p_deleteweight )
+    {
+        m_insertweight = p_insertweight;
+        m_replaceweight = p_replaceweight;
+        m_deleteweight = p_replaceweight;
+    }
+
+
+    @Override
+    public final double calculate( final Stream<? extends ITerm> p_first, final Stream<? extends ITerm> p_second )
+    {
+        return this.levenshtein(
             p_first.map( Object::toString ).collect( Collectors.joining( "" ) ),
             p_second.map( Object::toString ).collect( Collectors.joining( "" ) )
         );
@@ -56,12 +91,44 @@ public final class CLevenshteinDistance implements IMetric
      * @return distance
      * @see https://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Levenshtein_distance#Java
      */
-    private static double levenshtein( final String p_first, final String p_second )
+    private double levenshtein( final String p_first, final String p_second )
     {
+        // the array of distances
+        double[] l_cost = IntStream.range( 0, p_first.length() + 1 ).mapToDouble( i -> i ).toArray();
+        double[] l_newcost = new double[l_cost.length];
 
-        IntStream.range( 0, p_first.length() ).parallel().mapToLong( i -> p_first.charAt( i ) == p_second.charAt( i ) ? 0 : 1 ).sum()
+        for ( int j = 1; j < p_second.length() + 1; j++ )
+        {
+            l_newcost[0] = j;
+
+            // calculate cost of operation for all characters
+            for ( int i = 1; i < l_cost.length; i++ )
+                l_newcost[i] = min(
+                    l_cost[i - 1] + ( p_first.charAt( i - 1 ) == p_second.charAt( j - 1 ) ? 0 : m_replaceweight ),
+                    l_newcost[i - 1] + m_deleteweight,
+                    l_cost[i] + m_insertweight
+                );
+
+            final double[] l_swap = l_cost;
+            l_cost = l_newcost;
+            l_newcost = l_swap;
+        }
+
+        return l_cost[p_first.length()];
+    }
 
 
+    /**
+     * returns the minimum of three elemens
+     *
+     * @param p_first first value
+     * @param p_second second value
+     * @param p_third third value
+     * @return minimum
+     */
+    private static double min( final double p_first, final double p_second, final double p_third )
+    {
+        return Math.min( Math.min( p_first, p_second ), p_third );
     }
 
 
