97663ed8b1b22672fefce31784194f955acfc017
Philipp Kraus <philipp.kraus@flashpixx.de>
2017-03-07 09:33:40 +0100

    fix documentation

--- src/main/java/org/lightjason/agentspeak/action/buildin/math/blas/matrix/CTwoNorm.java
+++ src/main/java/org/lightjason/agentspeak/action/buildin/math/blas/matrix/CTwoNorm.java
@@ -40,8 +40,8 @@ import java.util.List;
  * Calculates for each input matrix the
  * two-norm and returns the value, the action never
  * fails
- * code [N1|N2] = math/blas/matrix/twonorm(M1,M2); @endcode
  *
+ * @code [N1|N2] = math/blas/matrix/twonorm(M1,M2); @endcode
  * @see https://en.wikipedia.org/wiki/Matrix_norm
  */
 public final class CTwoNorm extends IAlgebra
