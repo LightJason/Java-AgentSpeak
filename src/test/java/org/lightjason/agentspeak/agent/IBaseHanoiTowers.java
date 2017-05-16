3aa2fe6356928d6af2c654e32268b78dabb71fb3
Philipp Kraus <philipp.kraus@flashpixx.de>
2017-05-16 21:26:53 +0200

    add test printing

--- src/test/java/org/lightjason/agentspeak/agent/IBaseHanoiTowers.java
+++ src/test/java/org/lightjason/agentspeak/agent/IBaseHanoiTowers.java
@@ -45,6 +45,8 @@ import org.lightjason.agentspeak.language.variable.IVariable;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.text.MessageFormat;
 import java.util.Collections;
 import java.util.List;
@@ -68,6 +70,10 @@ import static org.junit.Assert.assertTrue;
 @SuppressWarnings( "serial" )
 abstract class IBaseHanoiTowers extends IBaseTest
 {
+    /**
+     * enable printing of test-data
+     */
+    private static final boolean PRINTENABLE = Files.exists( Paths.get( "agentprinting.conf" ) );
     /**
      * agent map
      */
@@ -158,7 +164,8 @@ abstract class IBaseHanoiTowers extends IBaseTest
     {
         while ( m_running.get() )
         {
-            //System.out.println( MessageFormat.format( "\ntower configuration: {0}", m_tower ) );
+            if (PRINTENABLE)
+                System.out.println( MessageFormat.format( "\ntower configuration: {0}", m_tower ) );
             m_agents.values()
                     .parallelStream()
                     .forEach( j -> {
@@ -234,10 +241,12 @@ abstract class IBaseHanoiTowers extends IBaseTest
                             new CTowerPush( 0.33 ),
                             new CTowerPop(),
                             new CTowerSize(),
-                            new CStop(),
-                            new CEmptyPrint()
+                            new CStop()
                         ),
-                        p_action
+                        Stream.concat(
+                            p_action,
+                            PRINTENABLE ? Stream.of() : Stream.of( new CEmptyPrint() )
+                        )
                     ),
                     CCommon.actionsFromPackage()
                 ).collect( Collectors.toSet() ),
