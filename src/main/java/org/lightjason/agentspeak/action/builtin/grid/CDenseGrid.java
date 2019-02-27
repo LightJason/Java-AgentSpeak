f4a9916a5c61812be10a2dfacce72fdea013a966
Philipp Kraus <philipp.kraus@flashpixx.de>
2019-02-27 13:41:20 +0100

    grid actions

---
+++ src/main/java/org/lightjason/agentspeak/action/builtin/grid/CDenseGrid.java
@@ -0,0 +1,74 @@
+/*
+ * @cond LICENSE
+ * ######################################################################################
+ * # LGPL License                                                                       #
+ * #                                                                                    #
+ * # This file is part of the LightJason AgentSpeak(L++)                                #
+ * # Copyright (c) 2015-19, LightJason (info@lightjason.org)                            #
+ * # This program is free software: you can redistribute it and/or modify               #
+ * # it under the terms of the GNU Lesser General Public License as                     #
+ * # published by the Free Software Foundation, either version 3 of the                 #
+ * # License, or (at your option) any later version.                                    #
+ * #                                                                                    #
+ * # This program is distributed in the hope that it will be useful,                    #
+ * # but WITHOUT ANY WARRANTY; without even the implied warranty of                     #
+ * # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                      #
+ * # GNU Lesser General Public License for more details.                                #
+ * #                                                                                    #
+ * # You should have received a copy of the GNU Lesser General Public License           #
+ * # along with this program. If not, see http://www.gnu.org/licenses/                  #
+ * ######################################################################################
+ * @endcond
+ */
+
+package org.lightjason.agentspeak.action.builtin.grid;
+
+import cern.colt.matrix.tobject.impl.DenseObjectMatrix2D;
+import com.codepoetics.protonpack.StreamUtils;
+import org.lightjason.agentspeak.action.IBaseAction;
+import org.lightjason.agentspeak.common.IPath;
+import org.lightjason.agentspeak.language.CCommon;
+import org.lightjason.agentspeak.language.CRawTerm;
+import org.lightjason.agentspeak.language.ITerm;
+import org.lightjason.agentspeak.language.execution.IContext;
+import org.lightjason.agentspeak.language.fuzzy.IFuzzyValue;
+
+import javax.annotation.Nonnegative;
+import javax.annotation.Nonnull;
+import java.util.List;
+import java.util.stream.Stream;
+
+
+public final class CDenseGrid extends IBaseAction
+{
+    /**
+     * action name
+     */
+    private static final IPath NAME = namebyclass( CDenseGrid.class, "grid" );
+
+    @Nonnull
+    @Override
+    public IPath name()
+    {
+        return NAME;
+    }
+
+    @Nonnegative
+    @Override
+    public int minimalArgumentNumber()
+    {
+        return 1;
+    }
+
+    @Nonnull
+    @Override
+    public Stream<IFuzzyValue<?>> execute( final boolean p_parallel, @Nonnull final IContext p_context,
+                                           @Nonnull final List<ITerm> p_argument, @Nonnull final List<ITerm> p_return )
+    {
+        StreamUtils.windowed( CCommon.flatten( p_argument ), 2, 2 )
+                   .map( i -> new DenseObjectMatrix2D( i.get( 0 ).<Number>raw().intValue(), i.get( 1 ).<Number>raw().intValue() ) )
+                   .forEach( i -> p_return.add( CRawTerm.of( i ) ) );
+
+        return Stream.of();
+    }
+}
