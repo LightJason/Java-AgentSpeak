a3d8ffac697a43c41bf41fb9f3e4a486a5340137
Philipp Kraus <philipp.kraus@flashpixx.de>
2019-04-13 14:52:22 +0200

    add fuzzy value test

---
+++ src/test/java/org/lightjason/agentspeak/language/fuzzy/TestCFuzzyValue.java
@@ -0,0 +1,55 @@
+/*
+ * @cond LICENSE
+ * ######################################################################################
+ * # LGPL License                                                                       #
+ * #                                                                                    #
+ * # This file is part of the LightJason                                                #
+ * # Copyright (c) 2015-19, LightJason (info@lightjason.org)                            #
+ * # This program is free software: you can redistribute it and/or modify               #
+ * # it under the terms of the GNU Lesser General Public License as                     #
+ * # published by the Free Software Foundation, either version 3 of the                 #
+ * # License, or (at your option) any later version.                                    #
+ * #                                                                                    #
+ * # This program is distributed in the hope that it will be useful,                    #
+ * # but WITHOUT ANY WARRANTY; without even the implied warranty of                     #
+ * # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                      #
+ * # GNU Lesser General Public License for more details.                                #
+ * #                                                                                    #
+ * # You should have received a copy of the GNU Lesser General Public License           #
+ * # along with this program. If not, see http://www.gnu.org/licenses/                  #
+ * ######################################################################################
+ * @endcond
+ */
+
+package org.lightjason.agentspeak.language.fuzzy;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.lightjason.agentspeak.language.fuzzy.set.ECrisp;
+import org.lightjason.agentspeak.testing.IBaseTest;
+
+
+/**
+ * test fuzzy value
+ */
+public final class TestCFuzzyValue extends IBaseTest
+{
+    /**
+     * test fuzzy-value error
+     */
+    @Test( expected = IllegalArgumentException.class )
+    public void valueerror()
+    {
+        CFuzzyValue.of( ECrisp.TRUE, 10 );
+    }
+
+    /**
+     * test fuzzy-value tostring
+     */
+    @Test
+    public void valuetostring()
+    {
+        Assert.assertEquals( "FALSE(0,35)", CFuzzyValue.of( ECrisp.FALSE, 0.35 ).toString() );
+    }
+
+}
